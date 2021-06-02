8b64f4bbf6d468d04715c70e3bd6b4bf82f70fe1
Jean-Baptiste MERIAUX <jean-baptiste.meriaux@diiage.org>
2021-06-02 12:32:16 +0200

    mise en ligne avant système de traduction

--- Program.cs
+++ Program.cs
@@ -12,14 +12,24 @@ namespace MariageJBMariana
 {
 	public class Program
 	{
+		public static Country Language { get; set; }
+
 		public static async Task Main(string[] args)
 		{
+			Language = Country.France;
+
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			await builder.Build().RunAsync();
+		}
+
+		public enum Country
+		{
+			France,
+			Colombia
 		}
 	}
 }
